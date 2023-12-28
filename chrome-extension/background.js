0ba1985acde51d0d0fe625cf8416599b91e1fcfc
Engin Karataş <enginkaratas99@gmail.com>
2023-12-29 00:50:03 +0300

    create theme switcher local storage function

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -2,15 +2,28 @@
 // function install_notice() {
 //     if (localStorage.getItem('install_time'))
 //         return;
+
+// v3.0.0     
 installNotice();
 function installNotice() {
   chrome.storage.local.get("install_time", ({ install_time }) => {
-    // the callback runs asynchronously so you should use the value here
     if (install_time) {
       return;
     }
     var now = new Date().getTime();
     chrome.storage.local.set({ install_time: now });
     chrome.tabs.create({ url: `welcome-sadai.html` }, function (tab) {});
+  });
+}
+
+themeColorSwitcher();
+function themeColorSwitcher() {
+  chrome.storage.local.get("theme", ({ theme }) => {
+    if (theme == "dark") {
+      chrome.storage.local.set({ theme: "light" });
+    }
+    else {
+      chrome.storage.local.set({ theme: "dark" });
+    }
   });
 }
